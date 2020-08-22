8e430cbcd21be7a67a0281ce17651115766d15e8
gabriel-bpro <47447526+gabriel-bpro@users.noreply.github.com>
2020-08-21 21:06:21 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,7 +1,26 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
-
 # All sklearn Transforms must have the `transform` and `fit` methods
+class StudentCategory(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        def classificaFaltas(elem):
+            if elem <= 3:
+                return 1
+            elif elem <= 6:
+                return 0
+            else:
+                return -1
+        
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        # Retornamos um novo dataframe com as colunas FALTAS e TEM_REPROVACAO categorizadas
+        data["FALTAS"] = data.iloc[:,10].apply(classificaFaltas)
+        data["TEM_REPROVACAO"] = data.iloc[:,0:4].apply(lambda row: bool(row[0] or row[1] or row[2] or row[3]), axis=1)
+        
+        return data
+
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
